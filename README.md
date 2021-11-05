2aa66686f6d3593505d593e10a576543aeef146d
Martin <mastodon0@users.noreply.github.com>
2021-11-05 21:37:44 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+```tsc -b tsconfig.json --watch --generateTrace tracing_slow``` for slow watch
+
+```tsc -p tsconfig.json --watch --generateTrace tracing_fast``` for fast watch
---
+++ index.ts
@@ -0,0 +1,11 @@
+import * as file1 from './files/file1';
+import * as file2 from './files/file2';
+import * as file3 from './files/file3';
+import * as file4 from './files/file4';
+import * as file5 from './files/file5';
+import * as file6 from './files/file6';
+import * as file7 from './files/file7';
+import * as file8 from './files/file8';
+import * as file9 from './files/file9';
+
+console.log(file1.foo(), file2.foo(), file3.foo(), file4.foo(), file5.foo(), file6.foo(), file7.foo(), file8.foo(), file9.foo());
